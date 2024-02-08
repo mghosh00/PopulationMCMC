ef873a36a2a4dc4d19198e8bc9d2fa99cd2f1fd7
Matthew Ghosh <matthew.ghosh88@gmail.com>
2024-02-08 10:50:17 +0000

    Update docstrings

--- population_mcmc/ode_system.py
+++ population_mcmc/ode_system.py
@@ -11,9 +11,8 @@ import scipy.integrate as si
 
 class ODESystem:
     """This class takes in the RHS of a system of ODEs in the form:
-        y'(t) = f(y, t; \theta), y(0) = y_{0},
-    where y, f, y_{0} are n-dimensional vectors, t is a scalar and \theta is an
-    m-dimensional parameter vector.
+    y'(t) = f(y, t; \theta), y(0) = y_{0}, where y, f, y_{0} are n-dimensional
+    vectors, t is a scalar and \theta is an m-dimensional parameter vector.
 
     """
 
@@ -24,11 +23,13 @@ class ODESystem:
         Parameters
         ----------
         rhs : typing.Callable
-            A function f : R^{n} x R -> R^{n} which takes in a vector y and time t
-            and returns the RHS of the ODE system, given parameters \theta.
+            A function f : R^{n} x R -> R^{n} which takes in a vector y and
+            time t and returns the RHS of the ODE system, given parameters
+            \theta.
         y_init : np.array
-
+            The initial values of the ODE to be passed to the system.
         times : np.array
+            The times to be used for the numerical solution.
         """
         pass
 
